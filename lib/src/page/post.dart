b2db1a0ca372617a616049a10622bbd49bd43ab3
Long9725 <develop9725@gmail.com>
2022-01-10 22:59:29 +0900

    Image swiper

--- lib/src/page/post.dart
+++ lib/src/page/post.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
-class PostPage extends StatelessWidget {
+class PostPage extends StatefulWidget {
+  const PostPage({Key? key}) : super(key: key);
+
+  @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  int photoIndex = 0;
+
+  List<String> photos = [
+    'assets/photos/post1.jpg',
+    'assets/photos/post2.jpg',
+    'assets/photos/post3.jpg'
+  ];
+
+  void _previousImage() {
+    setState(() {
+      photoIndex = photoIndex > 0 ? photoIndex - 1 : 0;
+    });
+  }
+
+  void _nextImage() {
+    setState(() {
+      photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Center(
-        child: Text("Post",
-            style: TextStyle(
-              color: Colors.black,
-            )),
-      ),
-    );
+        body: Container(
+            height: 300,
+            child: new Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return new Image.asset(photos[index], fit: BoxFit.fill,);
+              },
+              itemCount: photos.length,
+              pagination: new SwiperPagination(),
+            )));
   }
 }
