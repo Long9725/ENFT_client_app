32d70f730e8154f3d5fe9659cd7e9d957eda56d1
Long9725 <develop9725@gmail.com>
2022-02-09 08:01:01 +0900

    Add QR code

--- lib/src/screen/ticket.dart
+++ lib/src/screen/ticket.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:simple_animations/simple_animations.dart';
 import 'package:supercharged/supercharged.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import 'package:blue/src/helper.dart';
 
@@ -17,12 +18,16 @@ class TicketScreen extends StatefulWidget {
 }
 
 class TicketScreenState extends State<TicketScreen>
-    with TickerProviderStateMixin{
+    with TickerProviderStateMixin {
   final PageController _pageController = PageController(viewportFraction: 0.8);
+
   late AnimationController _controller;
   late Animation<Color?> animationOne;
   late Animation<Color?> animationTwo;
+
+  final double _scaleFactor = 0.8;
   int _selectedIndex = 0;
+  var _currPageValue = 0.0;
 
   @override
   void initState() {
@@ -30,9 +35,9 @@ class TicketScreenState extends State<TicketScreen>
 
     _controller =
         AnimationController(duration: const Duration(seconds: 4), vsync: this);
-    animationOne = ColorTween(begin: kPrimaryColor, end: kPrimaryColor.shade100)
+    animationOne = ColorTween(begin: kPrimaryColor, end: kPrimaryColor.shade300)
         .animate(_controller);
-    animationTwo = ColorTween(begin: kPrimaryColor.shade100, end: kPrimaryColor)
+    animationTwo = ColorTween(begin: kPrimaryColor.shade300, end: kPrimaryColor)
         .animate(_controller);
 
     _controller.repeat(max: 1);
@@ -45,6 +50,12 @@ class TicketScreenState extends State<TicketScreen>
         _controller.forward();
       }
     });
+
+    _pageController.addListener(() {
+      setState(() {
+        _currPageValue = _pageController.page!;
+      });
+    });
   }
 
   @override
@@ -52,6 +63,7 @@ class TicketScreenState extends State<TicketScreen>
     _controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width * 0.8;
@@ -69,34 +81,160 @@ class TicketScreenState extends State<TicketScreen>
         controller: _pageController,
         itemCount: 3,
         itemBuilder: (BuildContext context, int index) {
-          return AnimatedBuilder(animation: _controller, builder: (_,child) {
-            return ShaderMask(
-              child: Image(
-                image: AssetImage("assets/backgrounds/ticket.png"),
-              ),
-              shaderCallback: (Rect bounds) {
-                return LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                    colors: [animationOne.value!, animationTwo.value!])
-                    .createShader(bounds);
-              },
-              blendMode: BlendMode.srcATop,
-            );
-          });
-          //   Container(
-          //     decoration: BoxDecoration(
-          //     image: DecorationImage(
-          //     fit: BoxFit.fitHeight,
-          //     image: AssetImage(
-          //     "assets/backgrounds/ticket.png",
-          // )),
-          // ),);
+          return buildTicket(index, height, width);
         },
       ),
     )));
   }
 
+  Widget buildTicket(int index, double height, double width) {
+    Matrix4 matrix = Matrix4.identity();
+    var currOpacity = 1.0;
+    if (index == _currPageValue.floor()) {
+      var currScale = 1.0 - (_currPageValue - index) * (1.0 - _scaleFactor);
+      var currTrans = height * (1.0 - currScale) / 2.0;
+      matrix = Matrix4.diagonal3Values(1.0, currScale, 1.0)
+        ..setTranslationRaw(0.0, currTrans, 0.0);
+      currOpacity = currScale;
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1.0 - _scaleFactor);
+      var currTrans = height * (1.0 - currScale) / 2.0;
+      matrix = Matrix4.diagonal3Values(1.0, currScale, 1.0)
+        ..setTranslationRaw(0.0, currTrans, 0.0);
+      currOpacity = currScale - 0.2;
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index - 1) * (1.0 - _scaleFactor);
+      var currTrans = height * (1.0 - currScale) / 2.0;
+      matrix = Matrix4.diagonal3Values(1.0, currScale, 1.0)
+        ..setTranslationRaw(0.0, currTrans, 0.0);
+      currOpacity = currScale - 0.2;
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1.0, currScale, 1.0)
+        ..setTranslationRaw(0.0, height * (1.0 - _scaleFactor) / 2.0, 0.0);
+      currOpacity = currScale - 0.2;
+    }
+
+    return Transform(
+        transform: matrix,
+        child: Opacity(
+            opacity: currOpacity,
+            child: AnimatedBuilder(
+                animation: _controller,
+                builder: (_, child) {
+                  return Stack(alignment: Alignment.center, children: [
+                    ShaderMask(
+                      child: Image(
+                        image: AssetImage("assets/backgrounds/ticket.png"),
+                      ),
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                            colors: [
+                              animationOne.value!,
+                              animationTwo.value!
+                            ]).createShader(bounds);
+                      },
+                      blendMode: BlendMode.srcATop,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding * 3),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          Expanded(
+                              child: Align(
+                            alignment: Alignment.topRight,
+                            child: Text("HK",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40.0,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 4.0)),
+                          )),
+                          Text(
+                            "ENFT\n헬스장 이용권",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.normal),
+                          ),
+                          SizedBox(
+                            height: kDefaultPadding / 2,
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.favorite, color: Colors.white),
+                              SizedBox(
+                                height: 0,
+                                width: kDefaultPadding / 2,
+                              ),
+                              Expanded(
+                                  child: Text("아주대학교 헬스장",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ))),
+                              Icon(Icons.share, color: Colors.white)
+                            ],
+                          ),
+                          SizedBox(
+                            height: kDefaultPadding * 2,
+                          ),
+                          Divider(
+                            thickness: 1,
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            height: kDefaultPadding / 8,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                  child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("전자출입\nQR  코드",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 26.0,
+                                          fontWeight: FontWeight.bold)),
+                                  Text("오늘도 안전한\n편리한 헬스장",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16.0,
+                                      )),
+                                ],
+                              )),
+                              QrImage(
+                                data: "https://www.naver.com",
+                                backgroundColor: Colors.white,
+                                size: 125,
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: kDefaultPadding / 8,
+                          ),
+                          Divider(
+                            thickness: 1,
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            height: kDefaultPadding / 3,
+                          ),
+                        ],
+                      ),
+                    )
+                  ]);
+                })));
+  }
 // int _selectedIndex = 0;
 // final PageController _pageController = PageController(viewportFraction: 0.8);
 // var _currPageValue = 0.0;
