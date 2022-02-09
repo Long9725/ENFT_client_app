23655e9c0029c8035da4bad082d0bfb35b40a7c5
Long9725 <develop9725@gmail.com>
2022-02-09 10:14:27 +0900

    Back of ticket

--- lib/src/screen/ticket.dart
+++ lib/src/screen/ticket.dart
@@ -303,9 +303,35 @@ class TicketScreenState extends State<TicketScreen>
                       margin: const EdgeInsets.symmetric(
                           horizontal: kDefaultPadding * 3),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          
+                          SizedBox(
+                            height: kDefaultPadding,
+                          ),
+                          Expanded(
+                              child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text("HK",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 40.0,
+                                        fontWeight: FontWeight.bold,
+                                      )))),
+                          Row(
+                            children: [
+                              Expanded(
+                                  child: buildColumnTextItem(
+                                      "등록날짜", "2022.02.09", false)),
+                              buildColumnTextItem("만료날짜", "2022.08.09", false),
+                            ],
+                          ),
+                          SizedBox(
+                            height: kDefaultPadding * 2,
+                          ),
+                          buildColumnTextItem("남은기간", "D+180", true),
+                          SizedBox(
+                            height: kDefaultPadding * 3,
+                          ),
                           Row(
                             children: [
                               Expanded(
@@ -346,5 +372,29 @@ class TicketScreenState extends State<TicketScreen>
                     )
                   ]);
                 })));
+  }
+
+  Widget buildColumnTextItem(String title, String text, bool isBool) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
+        const SizedBox(
+          height: kDefaultPadding / 4,
+          width: 0,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+              fontWeight: isBool ? FontWeight.bold : FontWeight.normal,
+              fontSize: isBool ? 30.0 : 16.0,
+              color: Colors.white),
+        )
+      ],
+    );
   }
 }
